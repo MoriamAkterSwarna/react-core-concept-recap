1afb5d922b86c45c3f85fcd9d9126fc5be22d6c1
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2025-03-03 21:20:08 +0600

    Code For Video 39-8 added

---
+++ src/Post.jsx
@@ -0,0 +1,18 @@
+import "./Post.css";
+
+export default function Post({ post }) {
+  const { title, body, userId, id } = post;
+
+  return (
+    <div className="post">
+      <h3>Title: {title} </h3>
+      <p>
+        <small>userId: {userId} </small>
+      </p>
+      <p>
+        <small>postId: {id} </small>
+      </p>
+      <p>{body}</p>
+    </div>
+  );
+}
--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,26 +1,29 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Post from "./Post";
 
 export default function Posts() {
+  const [posts, setPosts] = useState([]);
 
-    const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  return (
+    <div>
+      <h3>Posts: {posts.length} </h3>
 
 
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((response) => response.json())
-        .then((data) => setPosts(data));
-    }, []);
+      {
+        posts.map((post) => ( <Post key={post.id} post={post} /> ))
+      }
+    </div>
+  );
+}
 
-
-    return (
-        <div>
-        <h3>Posts:  </h3>
-        </div>
-    );
-    }
-
-    /* 
+/* 
 
     1. Create a state to store the data 
     2. create useEffect with no dependencies 
